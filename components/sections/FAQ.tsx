f5fd5547596ac841646adb55be9237c38e9c7339
Owarino777 <malikcheikhpro14@gmail.com>
2025-12-09 23:26:23 +0100

    feat: Mettre à jour les styles des sections CTA, FAQ, Features, Happy Customers et Hero avec de nouvelles couleurs et ajustements de mise en page

--- components/sections/FAQ.tsx
+++ components/sections/FAQ.tsx
@@ -8,19 +8,19 @@ export default function FAQ() {
     const faqs = [
         {
             id: 1,
-            question: "Question 1 : Mon animal nécessite des soins médicaux, puis-je utiliser Mamipet ?",
+            question: "Mon animal nécessite des soins médicaux, puis-je utiliser Mamipet ?",
             answer:
                 "Absolument ! C’est même notre spécialité. Tous nos pet‑sitters passent un questionnaire de compétences détaillé sur le type de soins, voies de prises, types de gestes et besoins chroniques avant de valider leur profil. Tout cela est vérifié par notre équipe interne, puis notre assurance couvre chaque prestation au domicile du patient.",
         },
         {
             id: 2,
-            question: "Question 2 : Comment vérifiez-vous les compétences des pet‑sitters ?",
+            question: "Comment vérifiez-vous les compétences des pet‑sitters ?",
             answer:
                 "Chaque pet‑sitter doit soumettre des preuves de formation et d’expérience spécifiques pour les soins médicaux des animaux. Nous analysons les documents fournis et nous réalisons des entretiens individuels. De plus, les propriétaires laissent des avis détaillés après chaque garde.",
         },
         {
             id: 3,
-            question: "Question 3 : Que se passe‑t‑il en cas de problème pendant la garde ?",
+            question: "Que se passe‑t‑il en cas de problème pendant la garde ?",
             answer:
                 "Notre assurance inclut un accompagnement médical d’urgence et une couverture soigneur unique par mission. En cas de souci, nous contactons immédiatement votre vétérinaire référent, puis nous vous informons en direct de chaque décision prise. Tous les incidents sont documentés et pris en charge.",
         },
